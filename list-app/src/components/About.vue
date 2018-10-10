5a028342efa7802e2b74b7126d54c4063bd8ca77
Artem Rosnovskiy <artem@rosnovsky.us>
2018-10-10 11:50:07 -0700

    folder structure updated

---
+++ list-app/src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+      About Page
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum iste, error in eum ducimus voluptatum a maiores rem quos reiciendis nam, minus nisi ipsum, ratione perferendis! Eos molestiae autem praesentium!
+    </p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
