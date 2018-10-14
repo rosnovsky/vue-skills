4073e494935855fa0cdf55bb807007db74736095
Artem Rosnovskiy <inbox@podtema.com>
2018-10-14 16:07:43 -0700

    refactor

--- tab-tracker/server/src/app.js
+++ tab-tracker/server/src/app.js
@@ -14,5 +14,9 @@ require('./routes')(app);
 
 sequelize.sync().then(() => {
   app.listen(config.port);
+  // We console.log the fact that the server is running and on what port.
+  // Linter will complain about console.log unless we disable lint for the next line.
+  // Remove next 2 lines if you don't need server status/port information in the console anymore.
+  // eslint-disable-next-line
   console.log(`Server is running on ${config.port}`);
 });
